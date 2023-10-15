6c5ebf76ace8330394650bcb2bad48fec84b104e
Callum <callummorgan666@gmail.com>
2023-10-15 19:13:35 +0100

    create error handling for opening spreadsheet

--- run.py
+++ run.py
@@ -1,68 +1,47 @@
-import gspread
-from google.oauth2.service_account import Credentials
 import time
 import sys
+import random
+from os import system
 
-def slow_type(text, delay=0.05):
-    for char in text:
+from time import sleep
+from google.oauth2.service_account import Credentials
+import gspread
+
+
+def clear():
+    """
+    Clears the terminal.
+    See https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
+    """
+    system("clear")
+
+
+def t_print(message, delay=0.05):
+    """
+    Prints the passed string to the console, simulating a typewriter.
+    """
+    for char in message:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(delay)
+    print()
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('terminal-horror')
 
-def introduction():
-    """
-    Introduction for game with description of what the game is about
-    """
-    print("Welcome to the Terminal Horror")
-    haunted_house = """
-                                    .     .
-                                    !!!!!!!
-                            .       [[[|]]]    .
-                            !!!!!!!!|--_--|!!!!!
-                            [[[[[[[[\_(X)_/]]]]]
-                    .-.     /-_--__-/_--_-\-_--\\
-                    |=|    /-_---__/__-__-_\__-_\\
-                . . |=| ._/-__-__\===========/-__\_
-                !!!!!!!!!\========[ /]]|[[\ ]=====/
-                /-_--_-_-_[[[[[[[[[||==  == ||]]]]]]
-                /-_--_--_--_|=  === ||=/^|^\ ||== =|
-                /-_-/^|^\-_--| /^|^\=|| | | | ||^\= |
-            /_-_-| | |-_--|=| | | ||=|_|_|=||"|==|
-            /-__--|_|_|_-_-| |_|_|=||______=||_| =|
-            /_-__--_-__-___-|_=__=_.`---------'._=_|__
-            /-----------------------\===========/-----/
-        ^^^\^^^^^^^^^^^^^^^^^^^^^^[[|]]|[[|]]=====/
-            |.' ..==::'"'::==.. '.[ /~~~~~\ ]]]]]]]
-            | .'=[[[|]]|[[|]]]=`._||==  =  || =\ ]
-            ||== =|/ _____ \|== = ||=/^|^\=||^\ ||
-            || == `||-----||' = ==|| | | |=|| |=||
-            ||= == ||:^s^:|| = == ||=| | | || |=||
-            || = = ||:___:||= == =|| |_|_| ||_|=||
-            _||_ = =||o---.|| = ==_||_= == =||==_||_
-            \__/= = ||:   :||= == \__/[][][][][]\__/
-            [||]= ==||:___:|| = = [||]\\//\\//\\[||]
-            }  {---'"'-----'"'- --}  {//\\//\\//}  {
-            __[==]__________________[==]\\//\\//\\[==]_
-        |`|~~~~|================|~~~~|~~~~~~~~|~~~~||
-        jgs|^| ^  |================|^   | ^ ^^ ^ |  ^ ||
-        \|//\\/^|/==============\|/^\\\^/^.\^///\\//|///
-        \\///\\\//===============\\//\\///\\\\////\\\/////
-        """
-    slow_type("In this text-based adventure game. You will be tasked with the responsibility of finding the ghost\n")
-    slow_type("The question is - which ghost could it be? It's your job to uncover the nature of the ghost that is lurking within the shadows\n")
-    slow_type("BE AWARE... The deeper you tread into the abyss of the mystery the ghost may get angrier tread carefully\n")
-    slow_type("Do you have what it takes? Or will you become another ghostly resident of the Terminal Horror Mansion...")
-
-
-introduction()
+try:
+    SHEET = GSPREAD_CLIENT.open('terminal-horror')
+except gspread.exceptions.SpreadsheetNotFound:
+    print("The 'terminal-horror' spreadsheet was not found.")
+    sys.exit(1)
+except ConnectionError as error:
+    print(f"An error occurred while opening the spreadsheet: {error}")
+    sys.exit(1)
